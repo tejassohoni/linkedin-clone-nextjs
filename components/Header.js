03caf5f69edb3290b697b268c5c0bd1c762366a7
Tejas Sohoni <tjs1597@gmail.com>
2022-01-28 22:20:50 +0530

    Added main page header and implemented dark mode

---
+++ components/Header.js
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HeaderLink from "./HeaderLink";
+import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+
+import { Avatar } from "@mui/material";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
+
+export default function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+  return (
+    <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg transition-all duration-200">
+      {/* Left */}
+      <div className="flex items-center space-x-2 w-full max-w-xs">
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+            )}
+          </>
+        )}
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+          <SearchRoundedIcon />
+          <input
+            placeholder="Search"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-gray-700/70 dark:placeholder-white/75 flex-grow "
+          />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={GroupIcon} text="My Network" feed />
+        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+
+        {/* Dark mode toggle */}
+        {mounted && (
+          <div
+            className={`bg-gray-700 dark:bg-gray-300 flex items-center h-6 w-12 px-0.5 py-0.5 rounded-full cursor-pointer flex-shrink-0 relative ${
+              resolvedTheme === "dark" ? "justify-end" : "justify-start"
+            }`}
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+          >
+            <span className="absolute left-0">🌑</span>
+            <motion.div
+              className="h-5 w-5 bg-white dark:bg-gray-700 rounded-full z-40"
+              layout
+              transition={spring}
+            />
+
+            <span className="absolute right-0">☀</span>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Linked In CLone</title>
+        <title>LinkedIn Clone</title>
         <meta name="description" content="Linked In Clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Linkedin clone
+      <Header />
     </div>
   );
 }
