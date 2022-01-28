e1e6e8a10ab89943b401dd756d0382908da0b351
Tejas Sohoni <tjs1597@gmail.com>
2022-01-28 11:28:04 +0530

    Created Home Page

---
+++ pages/home.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import React from "react";
+
+import ExploreIcon from "@mui/icons-material/Explore";
+import GroupIcon from "@mui/icons-material/Group";
+import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+
+import HeaderLink from "../components/HeaderLink";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="space-y-10 relative">
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="flex items-center justify-around py-4">
+        <div className="relative w-36 h-10">
+          <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+        </div>
+        <div className="flex items-center sm:divide-x divide-gray-300">
+          <div className="hidden sm:flex space-x-8 pr-4">
+            <HeaderLink Icon={ExploreIcon} text="Discover" />
+            <HeaderLink Icon={GroupIcon} text="People" />
+            <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
+            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+          </div>
+          <div className="pl-4">
+            <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-2 hover:border-2 transition-all duration-200 ease-out">
+              Sign In
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1 className="text-amber-800 text-3xl md:text-5xl max-w-xl !leading-snug pl-4 xl:pl-0">
+            Welcome to your professional community
+          </h1>
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Find a person you know</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRoundedIcon className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+        <div className="relative xl:absolute top-14 right-5 h-96 w-96 xl:h-[650px] xl:w-[650px]">
+          <Image src="https://rb.gy/vkzpzt" layout="fill" priority />
+        </div>
+      </main>
+    </div>
+  );
+}
